9419d89f42da4674d17e2fce6c17a3fe5f51ac12
paulkumpula <n2kupa00@students.oamk.fi>
2023-03-16 12:31:14 +0200

    first commit

---
+++ db.sql
@@ -0,0 +1,14 @@
+create database shoppinglist;
+
+use shoppinglist;
+
+create table item (
+    id int primary key auto_increment,
+    description varchar(255) not null,
+    amount smallint unsigned not null
+);
+
+insert into item (description,amount) values ('Test item',1);
+insert into item (description,amount) values ('Second test item',2);
+insert into item (description,amount) values ('Third test item',3);
+
